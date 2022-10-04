60ecc04f08ac63409f85f320591afa80b10a8750
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-04 15:08:29 +0900

    Use Bean for SecretKey Singleton

--- src/main/java/com/ll/exam/jwt/app/jwt/JwtProvider.java
+++ src/main/java/com/ll/exam/jwt/app/jwt/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.ll.exam.jwt.app.jwt;
 
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Component;
@@ -10,21 +11,11 @@ import java.util.Base64;
 
 @Component
 @EnableCaching
+@RequiredArgsConstructor
 public class JwtProvider {
-    private SecretKey cachedSecretKey;
-    @Value("${custom.jwt.secretKey}")
-    private String secretKeyPlain;
-
-    public SecretKey _getSecretKey() {
-        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
-        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
-    }
+    private final SecretKey jwtSecretKey;
 
     public SecretKey getSecretKey() {
-        if(cachedSecretKey == null) {
-            cachedSecretKey = _getSecretKey();
-        }
-
-        return cachedSecretKey;
+        return jwtSecretKey;
     }
 }
